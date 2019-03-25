a781f01d12d42a1171cf9a4f0b49c2b1d94c5f25
Wallace92 <mateuszwallace@gmail.com>
2019-03-25 18:49:06 +0100

    The Bisection method

--- Bisekcja.py
+++ Bisekcja.py
@@ -2,59 +2,66 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-class Bisection:
-    # Function for which the Bisection method operates
+def equation(x):
+    return 6*x**3 - 12*x ** 2 - 3 * x + 5
 
-    def equation(self, x):
-        return -x**4 + 6*x**3 - 12*x **2 + 3 *x + 5
 
-    def bis(self, a, b):
+def bisection(a, b, n=100):
+    roots = []
+    for j in range(0, n):
+        # If the function is not continuous break
+        if equation(a) * equation(b) > 0:
+            break
+        # Divide interval at half
+        x1 = (a + b) / 2
+        # If the root are find break
+        if equation(x1) == 0:
+            break
+        # Define new smaller interval [a, x1] and [x1, b] according with algorithm
+        elif equation(x1) * equation(a) < 0:
+            b = x1
+            roots.append(b)
+        else:
+            a = x1
+            roots.append(a)
+    # If the roots exist return it (the last element of the root list have the best precision
+    try:
+        return np.round(roots[len(roots)-1], 4)
 
-        roz = []
+    except IndexError:
+        pass
 
-        for i in range(0,10):
-            # Sprawdzamy warunek miejsca zerowego 
-            if (self.equation(a) * self.equation(b) > 0):
-                break
 
-            x1 = (a + b) / 2
+def interval(a, b):
 
-            if (self.equation(x1) == 0):
-                break
-            elif (self.equation(x1) * self.equation(a) < 0):
-                b = x1;
-                roz.append(b)
-            else:
-                a = x1;
-                roz.append(a)
-        try:
-            return np.round(roz[9],2)
-        except IndexError:
-            pass
+    interval_steps = np.linspace(a, b, 10)
+    results_of_scanning = []
 
-    def przedzial(self, a, b):
+    for k in interval_steps:
+        results_of_scanning.append(bisection(k, k + 1))
+        # return roots
+    return set(results_of_scanning)
 
-        L = np.linspace(a, b, 10)
-        lista = []
-        for i in L:
-            lista.append(self.bis(i,i+1))
-        return set(lista)
 
-rozw = Bisection().przedzial(-2, 2)
-print (rozw)
+# Find root withing the interval a and b
+a_initial = -5
+b_initial = 5
+root = interval(a_initial, b_initial)
+print(root)
 
-x = np.linspace(-5,5, 40)
+# For plot function
+X = np.linspace(a_initial, b_initial, 40)
 y = []
-for i in x:
-    y.append(Bisection().equation(i))
+for i in X:
+    y.append(equation(i))
 
-rozw = list(rozw)
-zero = np.zeros(len(rozw))
+root = list(root)
+y_zero = np.zeros(len(root))
 
-plt.plot(rozw,zero, 'o', ms=10)
-plt.plot(x,y)
-plt.axis([-5,5,-2,2])
-#plt.savefig('Bisekcja')
+plt.plot(root, y_zero, 'o', ms=10)
+plt.plot(X, y)
+plt.axis([a_initial, b_initial, min(y), max(y)+2])
+plt.savefig('bisection.png')
 plt.show()
 
 
